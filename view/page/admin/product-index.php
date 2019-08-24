d7a6086bfc82f53d8c0763d5cdd11c20ae4de5d8
Arix Wap <arix.wap@gmail.com>
2019-08-24 23:45:01 +0800

    build product page

--- view/page/admin/product-index.php
+++ view/page/admin/product-index.php
@@ -1,20 +1,37 @@
 <div class="container">
-    <a class="waves-effect waves-light btn blue lighten-2" href="<?=baseurl('admin/product-create')?>">
-        <i class="material-icons left">add</i>Product
-    </a>
-    <ul class="collection">
-        <?php foreach($products as $key => $product) : ?>
-            <li class="collection-item avatar">
-                <img src="<?=$product['images']?>" alt="<?=$product['slug']?>" class="circle">
-                <b><span class="title bold"><?=$product['name']?></span></b>
-                <br/>
-                <span class="quantity">(<?=number_format($product['quantity'])?>)</span>
-                <span class="price">Rp. <?=number_format($product['price'])?></span>
-                <div class="secondary-content">
-                    <a href="<?=baseurl('admin/product-edit/'.$product['id'])?>"><i class="material-icons blue-text text-lighten-1">edit</i></a>
-                    <a href="<?=baseurl('admin/product-delete/'.$product['id'])?>"><i class="material-icons red-text text-lighten-2">delete</i></a>
-                </div>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    <div class="row">
+        <div class="col l3 s12 m-b-1">
+            <a class="waves-effect waves-light btn blue lighten-2 btn-block" href="<?=baseurl('admin/product-create')?>">
+                <i class="material-icons left">add</i>New Product
+            </a>
+        </div>
+        <div class="col l3 s12 m-b-1">
+            <a class="waves-effect waves-light btn blue lighten-2 btn-block" href="<?=baseurl('admin/category')?>">
+                <i class="material-icons left">border_all</i>Category List
+            </a>
+        </div>
+        <div class="col l3 s12 m-b-1">
+            <a class="waves-effect waves-light btn blue lighten-2 btn-block" href="<?=baseurl('admin/order')?>">
+                <i class="material-icons left">receipt</i>Order List
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12">
+            <ul class="collection">
+                <?php foreach($products as $key => $product) : ?>
+                    <li class="collection-item avatar">
+                        <img src="<?=$product['images']?>" alt="<?=$product['slug']?>" class="circle">
+                        <b><span class="title bold"><?=$product['name']?></span></b><br/>
+                        <small class="quantity">Stock : <b><?=number_format($product['quantity'])?></b></small><br/>
+                        <small class="price">Price : <b>Rp. <?=number_format($product['price'])?></b></small>
+                        <div class="secondary-content">
+                            <a href="<?=baseurl('admin/product-edit/'.$product['id'])?>"><i class="material-icons blue-text text-lighten-1">edit</i></a>
+                            <a href="<?=baseurl('admin/product-delete/'.$product['id'])?>"><i class="material-icons red-text text-lighten-2">delete</i></a>
+                        </div>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </div>
 </div>
