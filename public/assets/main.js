171a7f1661ca3a30ce8cf7a349994723da277b0e
Arix Wap <arix.wap@gmail.com>
2019-08-25 18:26:45 +0800

    add git ignore

--- public/assets/main.js
+++ public/assets/main.js
@@ -13,4 +13,23 @@
  */
 $(document).ready(function(){
     $('select.materialize-select').formSelect();
+    $('textarea.character-counter, input.character-counter').characterCounter();
 });
+
+// $(document).ready(function (e){
+//     $("#uploadForm").on('submit',(function(e){
+//         e.preventDefault();
+//         $.ajax({
+//         url: "upload.php",
+//         type: "POST",
+//         data:  new FormData(this),
+//         contentType: false,
+//         cache: false,
+//         processData:false,
+//         success: function(data){
+//         $("#targetLayer").html(data);
+//         },
+//         error: function(){}
+//         });
+//     }));
+// });
